c6e512a33826ccc90f02b5ebc3247685d5bd9d43
AyushPatiTripathi <ayushtripathi218@gmail.com>
2025-08-19 00:07:17 +0530

    pagination

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render , get_object_or_404
 from django.http import HttpResponse 
 from .models import Blogpost
+from django.core.paginator import Paginator
+
 
 
 def blog(request):
-    posts = Blogpost.objects.all().order_by('-created_at') # newest  f[irst
+    posts_list = Blogpost.objects.all().order_by('-created_at') # newest  f[irst
+  
+    
+    paginator = Paginator(posts_list , 5)
+    
+    page_number = request.GET.get('page')
+    posts = paginator.get_page(page_number)
     return render(request,'blog/blog.html',{'posts': posts})
     
 def blog_detail(request, slug):
