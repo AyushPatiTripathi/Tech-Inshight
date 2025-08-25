b2ff9cadfb58473c504d94404af6e8ca87dc63e0
AyushPatiTripathi <ayushtripathi218@gmail.com>
2025-08-25 13:33:24 +0530

    search bar

--- blog/models.py
+++ blog/models.py
@@ -21,7 +21,7 @@ class Blogpost(models.Model):
     
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = sulgify(self.title)
+            self.slug = slugify(self.title)
         super().save(*args, **kwargs)
     def __str__(self):
         return self.title
--- blog/views.py
+++ blog/views.py
@@ -2,21 +2,30 @@ from django.shortcuts import render , get_object_or_404
 from django.http import HttpResponse 
 from .models import Blogpost
 from django.core.paginator import Paginator
-
+from django.db.models import Q
 
 
 def blog(request):
-    posts_list = Blogpost.objects.all().order_by('-created_at') # newest  f[irst
-  
+    query = request.GET.get("q","").strip()
+    posts = Blogpost.objects.all().order_by('-created_at') # newest  f[irst
+    if query:
+        posts = posts.filter(
+            Q(title__icontains=query) |
+            Q(content__icontains=query) |
+            Q(tags__icontains=query)
+    )
+ 
     
-    paginator = Paginator(posts_list , 5)
+    paginator = Paginator(posts , 5)
     
     page_number = request.GET.get('page')
-    posts = paginator.get_page(page_number)
-    return render(request,'blog/blog.html',{'posts': posts})
+    posts_ = paginator.get_page(page_number)
+    return render(request,'blog/blog.html',{'posts': posts_,
+                                            'query': query})
     
 def blog_detail(request, slug):
     post = get_object_or_404(Blogpost, slug=slug)
     return render(request, 'blog/detail.html', {'post':post})
+    
 
 
