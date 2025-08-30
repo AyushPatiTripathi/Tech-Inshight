34234f11a390b11033f52e77df82f0e43ce816b1
AyushPatiTripathi <ayushtripathi218@gmail.com>
2025-08-31 02:50:55 +0530

    added custom auth

--- blog/models.py
+++ blog/models.py
@@ -2,11 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.text import slugify 
 import math 
+from django.conf import settings
+
 
 class Blogpost(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True ,blank=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     content = models.TextField()
     image = models.ImageField(upload_to='blog_images/',blank=True,null =True )
     tags = models.CharField(max_length=100, blank=True)
